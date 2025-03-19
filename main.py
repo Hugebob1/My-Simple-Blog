e647d84f8b95fe3f6bb3fd9009af1d785a3ef024
Hugebob1 <01197882@pw.edu.pl>
2025-03-19 20:12:08 +0100

    final version with working posts

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+import requests
+from datetime import datetime
 app = Flask(__name__)
+
+ulr = "https://api.npoint.io/c3ca1ffbce863c6b5d8c"
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    response = requests.get(ulr)
+    data = response.json()
+    year = datetime.now().year
+    month = datetime.now().strftime("%B")
+    day = datetime.now().day
+    return render_template("index.html", posts=data, year=year, month=month, day=day)
 
 @app.route('/about')
 def about():
@@ -11,6 +20,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/post<post_id>')
+def place_post(post_id):
+    response = requests.get(ulr)
+    data = response.json()
+    year = datetime.now().year
+    for post in data:
+        if post['id'] == int(post_id):
+            return render_template("post.html", title=post['title'], subtitle=post['subtitle'], content=post['body'], img_url=post['image_url'], year=year)
 
 if __name__ == '__main__':
     app.run(debug=True)
